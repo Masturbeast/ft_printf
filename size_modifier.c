ecf389b70adf3f73feff47f7a815d099f348193e
Masturbeast <atep@e1r7p19.42.fr>
2019-04-15 21:57:47 +0200

    int size modiffier to check

--- size_modifier.c
+++ size_modifier.c
@@ -1,5 +1,17 @@
 #include "ft_printf.h"
 
-void    int_conv
-
-void    (t_printf *pf)
+void    int_size_modifier(t_printf *pf)
+{
+    /* 
+    to check: 
+    -long
+    -long lonng
+    -size_t
+    -intmax_t
+    -uintmax_t
+    -short
+    -unsigned int
+    -unsigned long
+    -unsigned short
+    */
+}
